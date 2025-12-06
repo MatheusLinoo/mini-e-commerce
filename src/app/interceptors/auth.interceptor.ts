05368ce7eda5ccc26133f33974c35a4692d70430
Aoife <imatt.lino@gmail.com>
2025-12-06 13:48:13 -0300

    feat(interceptors): implement auth interceptor again

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -1,17 +1,40 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AppComponent } from '../app.component';
 
-export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  
-  const token = localStorage.getItem('auth_token');
-
-  if (token) {
-    const cloned = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
-      }
-    });
-    return next(cloned);
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = localStorage.getItem('auth_token');
+    if (token) {
+      const cloned = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${token}`)
+      });
+      return next.handle(cloned);
+    }
+   
+    return next.handle(req);
   }
+}
 
-  return next(req);
-};
+@NgModule({
+  declarations: [
+   
+  ],
+  imports: [
+    BrowserModule,
+   
+    HttpClientModule,
+  
+  ],
+  providers: [
+   
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
